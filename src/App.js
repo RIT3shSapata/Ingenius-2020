976cebcb1ff79f9a393ab9ca6f44d5427da9c672
Ritesh Sapata <riteshsapata@gmail.com>
2020-10-30 22:49:28 +0530

    added Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Profile from './Components/Profile';
+import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
+          <Route path='/home'>
+            <Home />
+          </Route>
           <Route path='/profile'>
             <Navbar />
             <Profile />
