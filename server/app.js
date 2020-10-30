93d14e1e2e21cc05fa000bb4d846575c44f42cb2
Aditya Mehta <adumehta03@gmail.com>
2020-10-30 20:31:45 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator')
+require('dotenv').config();
+//import routes
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+
+const app = express();
+
+
+//database
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log("DATABASE CONNECTED"));
+
+
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator())
+app.use(cors())
+
+//routes middleware
+app.use(authRoutes); //use app.use("/api", authRoutes) to get deault "/api" in URL, example localhost:3000/api/signin, etc
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+})
