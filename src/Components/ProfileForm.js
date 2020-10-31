a137153c1fc3845bb18bda0fcb5406b4b0489c01
Ritesh Sapata <riteshsapata@gmail.com>
2020-10-31 10:56:17 +0530

    Added Profile Form

---
+++ src/Components/ProfileForm.js
@@ -0,0 +1,161 @@
+import { FormControl, MenuItem, Select, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import './ProfileForm.css';
+
+function ProfileForm() {
+  const [experience, setExperience] = useState(0);
+  useEffect(() => {
+    console.log(experience);
+  }, [experience]);
+  return (
+    <div className='Login'>
+      <div
+        className='account '
+        style={{ position: 'relative', textAlign: 'left', right: '35%' }}>
+        <form className='signup' onsubmit='return false' autocomplete='off'>
+          <h1>Login</h1>
+          <div class='signup__field'>
+            <input
+              className='signup__input'
+              type='text'
+              name='username'
+              id='username'
+              required
+            />
+            <label className='signup__label' for='username'>
+              LinkedIn
+            </label>
+          </div>
+
+          <div className='signup__field'>
+            <input
+              className='signup__input'
+              type='password'
+              name='password'
+              id='password'
+              required
+            />
+            <label className='signup__label' for='password'>
+              Github
+            </label>
+          </div>
+
+          <div>
+            <label>experience</label>
+            <select>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+            </select>
+          </div>
+
+          <div>
+            <label>Interest-1</label>
+            <select>
+              <option value={'Deep Learning'}>Deep Learning</option>
+              <option value={'Machine Learning'}>Machine Learning</option>
+              <option value={'Natural Language Processing'}>
+                Natural Language Processing
+              </option>
+              <option value={'Full Stack Web Development'}>
+                Full Stack Web Development
+              </option>
+              <option value={'Computer Vision'}>Computer Vision</option>
+              <option value={'App Development'}>App Development</option>
+              <option value={'Backend Development'}>Backend Development</option>
+              <option value={'Frontend Development'}>
+                Frontend Development
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label>Interest-2</label>
+            <select>
+              <option value={'Deep Learning'}>Deep Learning</option>
+              <option value={'Machine Learning'}>Machine Learning</option>
+              <option value={'Natural Language Processing'}>
+                Natural Language Processing
+              </option>
+              <option value={'Full Stack Web Development'}>
+                Full Stack Web Development
+              </option>
+              <option value={'Computer Vision'}>Computer Vision</option>
+              <option value={'App Development'}>App Development</option>
+              <option value={'Backend Development'}>Backend Development</option>
+              <option value={'Frontend Development'}>
+                Frontend Development
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label>Interest-3</label>
+            <select>
+              <option value={'Deep Learning'}>Deep Learning</option>
+              <option value={'Machine Learning'}>Machine Learning</option>
+              <option value={'Natural Language Processing'}>
+                Natural Language Processing
+              </option>
+              <option value={'Full Stack Web Development'}>
+                Full Stack Web Development
+              </option>
+              <option value={'Computer Vision'}>Computer Vision</option>
+              <option value={'App Development'}>App Development</option>
+              <option value={'Backend Development'}>Backend Development</option>
+              <option value={'Frontend Development'}>
+                Frontend Development
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label>Proficiency-1</label>
+            <select>
+              <option value={'Deep Learning'}>Deep Learning</option>
+              <option value={'Machine Learning'}>Machine Learning</option>
+              <option value={'Natural Language Processing'}>
+                Natural Language Processing
+              </option>
+              <option value={'Full Stack Web Development'}>
+                Full Stack Web Development
+              </option>
+              <option value={'Computer Vision'}>Computer Vision</option>
+              <option value={'App Development'}>App Development</option>
+              <option value={'Backend Development'}>Backend Development</option>
+              <option value={'Frontend Development'}>
+                Frontend Development
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label>Proficiency-2</label>
+            <select>
+              <option value={'Deep Learning'}>Deep Learning</option>
+              <option value={'Machine Learning'}>Machine Learning</option>
+              <option value={'Natural Language Processing'}>
+                Natural Language Processing
+              </option>
+              <option value={'Full Stack Web Development'}>
+                Full Stack Web Development
+              </option>
+              <option value={'Computer Vision'}>Computer Vision</option>
+              <option value={'App Development'}>App Development</option>
+              <option value={'Backend Development'}>Backend Development</option>
+              <option value={'Frontend Development'}>
+                Frontend Development
+              </option>
+            </select>
+          </div>
+
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileForm;
